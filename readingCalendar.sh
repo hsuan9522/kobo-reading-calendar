5d45a2f628fafb15e42b899382091b616cbad52f
Hsuan <jh558125@gmail.com>
2024-03-31 16:22:06 +0800

    Fix use python3

--- readingCalendar.sh
+++ readingCalendar.sh
@@ -2,7 +2,7 @@
 
 # echo 123
 cd /mnt/onboard/.adds/utils/analytics
-python_version=$(python --version 2>&1)
-echo "Python version: $python_version"
+# python_version=$(python --version 2>&1)
+# echo "Python version: $python_version"
 
-python readingCalendar.py
+python3 readingCalendar.py

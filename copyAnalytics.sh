30f0c49af97f0e489446d95944c071b4ad7d65b0
Hsuan <jh558125@gmail.com>
2024-04-24 21:19:19 +0800

    Modify fbink -q & 寫法修正

--- copyAnalytics.sh
+++ copyAnalytics.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fbink -pm -y -2 "Start Generating..."
+fbink -qpm -y -2 "Start Generating..."
 
 FORCE_ANALYZE=false
 FORCE_CONTENT=false
@@ -25,8 +25,6 @@ if [ $tmp_month -eq 0 ]; then
 fi
 LAST_MONTH=$(printf "%d-%02d" $tmp_year $tmp_month)
 
-# Set locking mode to NORMAL
-locking_mode_sql="PRAGMA locking_mode = NORMAL;"
 
 # Set journal mode to WAL (Write-Ahead Logging)
 journal_mode_sql="PRAGMA journal_mode = WAL;"
@@ -44,17 +42,17 @@ copyAnalyze() {
 
     INSERT OR IGNORE INTO target.Analytics
     SELECT 
-        Id,
+        t1.Id,
         t1.Timestamp,
-        Attributes,
-        Metrics, 
+        t1.Attributes,
+        t1.Metrics, 
         strftime('%Y-%m-%d', datetime(t1.Timestamp, '+08:00')) AS Date,
-        COALESCE(json_extract(Attributes, '$.title'),t2.Title) AS Title,
-        COALESCE(json_extract(Attributes, '$.author'),t2.Author) AS Author,
-        json_extract(Metrics, '$.SecondsRead') As ReadingTime
+        COALESCE(json_extract(t1.Attributes, '$.title'), t2.Title) AS Title,
+        COALESCE(json_extract(t1.Attributes, '$.author'), t2.Author) AS Author,
+        json_extract(t1.Metrics, '$.SecondsRead') As ReadingTime
     FROM src.AnalyticsEvents As t1
-    LEFT JOIN Books As t2 ON t2.ContentId = json_extract(Attributes, '$.volumeid')
-    WHERE Type = 'LeaveContent'
+    LEFT JOIN Books As t2 ON t2.ContentId = json_extract(t1.Attributes, '$.volumeid')
+    WHERE t1.Type = 'LeaveContent'
     AND t1.Timestamp > COALESCE((
         SELECT Timestamp
         FROM target.TimeInfo
@@ -78,23 +76,25 @@ copyContent() {
     ATTACH DATABASE '$MY_DB' AS target;
 
     INSERT OR IGNORE INTO target.Books
-    SELECT ContentID, Title, Attribution As Author, ___SyncTime As Timestamp FROM src.content As t1
-    WHERE ContentType = 6
-    AND ContentID NOT LIKE 'file://%'
-    AND isDownloaded = 'true'
-    AND Timestamp > COALESCE((SELECT Timestamp FROM TimeInfo WHERE Type = 'contentMaxTime'), 0);
-
+    SELECT ContentID, Title, Attribution AS Author, ___SyncTime AS Timestamp
+    FROM src.content AS t1
+    WHERE
+        ContentType = 6
+        AND ContentID NOT LIKE 'file://%'
+        AND isDownloaded = 'true'
+        AND Timestamp > COALESCE((SELECT Timestamp FROM TimeInfo WHERE Type = 'contentMaxTime'), 0);
+        
     DETACH DATABASE src;
     DETACH DATABASE target;
 
     INSERT OR REPLACE INTO TimeInfo(Timestamp, Type) VALUES('$CURRENT_TIMESTAMP', 'contentTime');
-    INSERT OR REPLACE INTO TimeInfo(Timestamp, Type) VALUES((SELECT MAX(Timestamp) FROM Books), 'contentMaxTime');
+    INSERT OR REPLACE INTO TimeInfo(Timestamp, Type) VALUES((SELECT MAX(Timestamp) FROM target.Books), 'contentMaxTime');
 EOF
 }
 
 calculateReading() {
-    month="${1:-$CURRENT_MONTH}"
-    export_file_name="$EXPORT$month.json"
+    local month="${1:-$CURRENT_MONTH}"
+    local export_file_name="${EXPORT}${month}.json"   
     # 計算每本書在同一天閱讀的時間
     $SQLITE "$MY_DB" <<EOF > $export_file_name
 .headers on
@@ -102,7 +102,7 @@ calculateReading() {
 
 SELECT Date, Title, Author,
 CAST(printf('%.1f', SUM(ReadingTime) / 60.0) AS REAL) AS TotalMinutesRead
-FROM Analytics t1
+FROM Analytics
 WHERE strftime('%Y-%m', datetime(Date)) = '$month'
 GROUP BY Date, Title
 HAVING TotalMinutesRead >= 1;
@@ -115,14 +115,29 @@ $SQLITE <<EOF > /tmp/sqlite_output.txt
 .headers off
 ATTACH DATABASE '$MY_DB' AS my;
 ATTACH DATABASE '$KOBO_DB' AS kobo;
+
+WITH kobo_max_content AS (
+    SELECT MAX(___SyncTime) AS max_content_time 
+    FROM kobo.content 
+    WHERE ContentType = 6 
+    AND ContentID NOT LIKE 'file://%' 
+    AND isDownloaded = 'true'
+),
+kobo_max_analytics AS (
+    SELECT MAX(Timestamp) AS max_analytics_time 
+    FROM kobo.AnalyticsEvents 
+    WHERE Type = 'LeaveContent'
+)
+
 SELECT 
     -- 存在自己 db 裡的最後時間
-    COALESCE(MAX(CASE WHEN Type = 'contentMaxTime' THEN Timestamp END), 0),
-    COALESCE(MAX(CASE WHEN Type = 'analyticsMaxTime' THEN Timestamp END), 0),
+    COALESCE(MAX(CASE WHEN Type = 'contentMaxTime' THEN Timestamp END), 0) AS my_content_time,
+    COALESCE(MAX(CASE WHEN Type = 'analyticsMaxTime' THEN Timestamp END), 0) AS my_analytics_time,
     -- Kobo 裡的最新資料
-    (SELECT MAX(___SyncTime) FROM kobo.content WHERE ContentType = 6 AND ContentID NOT LIKE 'file://%' AND isDownloaded = 'true' ),
-    (SELECT MAX(Timestamp) FROM kobo.AnalyticsEvents WHERE Type = 'LeaveContent') AS kobo_data
+    (SELECT max_content_time FROM kobo_max_content),
+    (SELECT max_analytics_time FROM kobo_max_analytics)
 FROM my.TimeInfo;
+
 DETACH DATABASE my;
 DETACH DATABASE kobo;
 EOF
@@ -139,11 +154,11 @@ if [ -n "$cs_analytics_time" ] || [ -n "$cs_content_time" ]; then
 
         # 與最新的時間不同才做
         if [ "$new_content_time" ">" "$cs_content_time" ] || [ "$FORCE_CONTENT" = true ]; then
-            fbink -pm -y -2 "Refresh Books map..."
+            fbink -qpm -y -2 "Refresh Books map..."
             copyContent
             echo "Refresh Books map..."
         else
-            fbink -pm -y -2 "There are no new books."
+            fbink -qpm -y -2 "There are no new books."
             echo "There are no new books."
         fi
     fi
@@ -151,12 +166,12 @@ if [ -n "$cs_analytics_time" ] || [ -n "$cs_content_time" ]; then
     if [ -n "$cs_analytics_time" ]; then
         # 與最新的時間不同才做
         if [ "$new_analytics_time" ">" "$cs_analytics_time" ] || [ "$FORCE_ANALYZE" = true ]; then
-            fbink -pm -y -2 "Generating analytics file..."
+            fbink -qpm -y -2 "Generating analytics file..."
             copyAnalyze
             calculateReading
             echo "Generating analytics file..."
         else
-            fbink -pm -y -2 "No new analytics events."
+            fbink -qpm -y -2 "No new analytics events."
             echo "No new analytics events."
         fi
     fi
@@ -169,11 +184,11 @@ fi
 
 last_month_file="$EXPORT$LAST_MONTH.json"
 if [ ! -f "$last_month_file" ]; then
-    fbink -pm -y -2 "Last month's file is generating..."
+    fbink -qpm -y -2 "Last month's file is generating..."
 
     calculateReading $LAST_MONTH
     echo "Last month's file is generating..."
 else
-    fbink -pm -y -2 "No need to generate last month's file."
+    fbink -qpm -y -2 "No need to generate last month's file."
     echo "No need to generate last month's file."
 fi
